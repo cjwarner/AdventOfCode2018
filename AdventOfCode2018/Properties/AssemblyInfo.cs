98542d1885951fc9bfdb9019cea6dae447853804
corey-warner <corey.warner@tattsgroup.com>
2018-12-03 11:52:31 +1000

    Add project files.

---
+++ AdventOfCode2018/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdventOfCode2018")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tatts Group Limited")]
+[assembly: AssemblyProduct("AdventOfCode2018")]
+[assembly: AssemblyCopyright("Copyright © Tatts Group Limited 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3fb2dc7d-9638-4153-a1b4-5e51949786d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
